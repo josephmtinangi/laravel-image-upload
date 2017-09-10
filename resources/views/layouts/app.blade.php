031ce52517e6a2e0745618fee3a2eeec259c74ac
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-09-10 10:24:30 +0000

    Create the upload form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//bootswatch.com/4-alpha/lux/bootstrap.css">
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+	@stack('style')
 </head>
 <body>
 
@@ -17,5 +19,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+
+	@stack('script')
 </body>
 </html>
